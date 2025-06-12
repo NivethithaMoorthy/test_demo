2518ebd98128429f4b39a3f51f41ec306467ebcf
Nivethitha <82363941+NivethithaMoorthy@users.noreply.github.com>
2025-06-12 05:13:19 +0000

    Student details added

---
+++ shopping/Students.java
@@ -0,0 +1,14 @@
+package shopping;
+
+public class Students 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Printing student details ");
+        System.out.println("Testing the shopping package");
+        System.out.println("Student Name: John Doe");
+        System.out.println("Student ID: 12345");
+        System.out.println("Course: Computer Science");
+    }
+    
+}
